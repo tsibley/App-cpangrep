c2182c591a359665ad7bc194bdd82bed511c79ec
Thomas Sibley <tsibley@cpan.org>
2013-04-27 22:16:20 -0700

    milla skeleton

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+App::cpangrep - Blah blah blah
+
+# SYNOPSIS
+
+    use App::cpangrep;
+
+# DESCRIPTION
+
+App::cpangrep is
+
+# AUTHOR
+
+Thomas Sibley <tsibley@cpan.org>
+
+# COPYRIGHT
+
+Copyright 2013- Thomas Sibley
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/App/cpangrep.pm
@@ -0,0 +1,39 @@
+package App::cpangrep;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::cpangrep - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use App::cpangrep;
+
+=head1 DESCRIPTION
+
+App::cpangrep is
+
+=head1 AUTHOR
+
+Thomas Sibley E<lt>tsibley@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013- Thomas Sibley
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
